151997614abe8314addd488641a105985f69c34d
TurtleBr0 <jacobheier@gmail.com>
2023-04-27 21:05:02 -0700

    homework

--- rest-rant/index.js
+++ rest-rant/index.js
@@ -8,11 +8,25 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-  res.send('Home')
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+      }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+      }]
+      
+  res.render('places/index', {places})
 })
 
 app.get('*', (req, res) => {
-  res.send('404 page')
-})
+    res.render('error404')
+  })  
 
 app.listen(process.env.PORT)
